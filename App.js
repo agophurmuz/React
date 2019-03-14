43dab9cabfe7651e877cbe91dc9e8b2fd8445cc3
Agop Hurmuz <agophurmuz@gmail.com>
2019-03-14 19:43:55 -0300

    overriding shared navigationOptions

--- App.js
+++ App.js
@@ -25,9 +25,16 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    const { params } = navigation.state;
+
     return {
-      title: navigation.getParam('otherParam', 'Details'),
+      title: params ? params.otherParam : 'A Nested Details Screen',
+      /* These values are used instead of the shared configuration! */
+      headerStyle: {
+        backgroundColor: navigationOptions.headerTintColor,
+      },
+      headerTintColor: navigationOptions.headerStyle.backgroundColor,
     };
   };
   render() {
